594500c80b66d3d6b0e0a453fdeddeeef1e10294
basil <basilvarghese012@gmail.com>
2026-05-22 04:43:01 +0530

    new 2026 last final bye

--- app/sections/projects.tsx
+++ app/sections/projects.tsx
@@ -299,7 +299,7 @@ export default function Projects() {
       className="relative z-10 bg-[#faf9f6]/40 border-t border-b border-slate-200/50 h-[300vh]"
     >
       {/* Sticky Viewport Container */}
-      <div className="sticky top-0 h-screen w-full flex flex-col items-center justify-start pt-12 sm:pt-20 overflow-hidden">
+      <div className="sticky top-0 h-screen w-full flex flex-col items-center justify-start pt-10 sm:pt-20 overflow-hidden">
         
         {/* Section Heading */}
         <div className="text-center z-20 px-4 shrink-0 mb-4 sm:mb-6">
